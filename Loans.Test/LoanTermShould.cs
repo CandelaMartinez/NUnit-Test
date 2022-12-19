244353bd3b6fcc0132e59499d61a06a0910f79cd
María Candela Martinez <macanmartinez@hotmail.com>
2022-12-19 10:53:56 +0100

    Asseting on float point values, tolerance.

--- Loans.Test/LoanTermShould.cs
+++ Loans.Test/LoanTermShould.cs
@@ -12,7 +12,8 @@ namespace Loans.Test
             //system under test
             var sut = new LoanTerm(1);
 
-            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+            Assert.That(sut.ToMonths(), Is.EqualTo(12),
+                "months should be 12 * number of years");
 
 
         }
@@ -40,6 +41,16 @@ namespace Loans.Test
             var b = new LoanTerm(2);
 
             Assert.That(a, Is.Not.EqualTo(b));
+        }
+        [Test]
+        public void Double()
+        {
+            double a = 1.0 / 3.0;
+
+            Assert.That(a, Is.EqualTo(0.33).Within(0.004));
+
+            Assert.That(a, Is.EqualTo(0.33).Within(10).Percent);
+
         }
     }
 }
