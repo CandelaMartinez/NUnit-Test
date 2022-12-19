f95c6dc828fbe1a2254a7d73481f24303f3899e3
María Candela Martinez <macanmartinez@hotmail.com>
2022-12-19 13:55:06 +0100

    Data driven tests

---
+++ Loans.Test/LoanRepaymentCalculatorShould.cs
@@ -0,0 +1,41 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+
+namespace Loans.Test
+{
+    public class LoanRepaymentCalculatorShould
+    {
+        [Test]
+        [TestCase(200_000, 6.5, 30, 1264.14)]
+        [TestCase(200_000, 10, 30, 1755.14)]
+        public void CalculateCorrectMonthlyRepayment(decimal principal,
+                                                     decimal interestRate,
+                                                     int termInYears,
+                                                     decimal expectedMonthlyPayment)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                                    new LoanAmount("USD", principal),
+                                    interestRate, new LoanTerm(termInYears));
+
+            Assert.That(monthlyPayment, Is.EqualTo(expectedMonthlyPayment));
+        }
+
+
+        [Test]
+        [TestCase(200_000, 6.5, 30, ExpectedResult = 1264.14)]
+        [TestCase(200_000, 10, 30, ExpectedResult= 1755.14)]
+        public decimal CalculateCorrectMonthlyRepayment_Simplified(decimal principal,
+                                                   decimal interestRate,
+                                                   int termInYears)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            return sut.CalculateMonthlyRepayment(
+                                    new LoanAmount("USD", principal),
+                                    interestRate, new LoanTerm(termInYears));
+
+        }
+    }
+}
